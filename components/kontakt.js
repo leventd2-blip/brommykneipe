5cb1602b62c69bc3d81f20a3a15a44f69c88a9ff
EAufDie1 <levent.d2@schule.bremen.de>
2026-07-17 19:02:22 +0000

    feat: setup clean text placeholders and vercel config

---
+++ components/kontakt.js
@@ -0,0 +1,13 @@
+export const kontaktDaten = {
+    infoTitel: "Platzhalter Info Titel",
+    infoText: "Platzhalter Info Text",
+    zeiten: [
+        { tage: "Platzhalter Tage 1", uhrzeit: "00:00 - 00:00 Uhr" },
+        { tage: "Platzhalter Tage 2", uhrzeit: "00:00 - 00:00 Uhr" },
+        { tage: "Platzhalter Tage 3", uhrzeit: "00:00 - 00:00 Uhr" }
+    ],
+    ruhetag: "Platzhalter Ruhetag",
+    adresse: "Platzhalter Name\nPlatzhalter Straße\nPlatzhalter Ort",
+    email: "platzhalter@email.de", 
+    telefon: "+491234567890"
+};
---
+++ components/speisen.js
@@ -0,0 +1,32 @@
+export const speisenKategorien = [
+    {
+        kategorieName: "Platzhalter Kategorie 1",
+        items: [
+            {
+                name: "Platzhalter Name Gericht 1",
+                description: "Platzhalter Beschreibung Gericht 1",
+                price: "0,00 €"
+            },
+            {
+                name: "Platzhalter Name Gericht 2",
+                description: "Platzhalter Beschreibung Gericht 2",
+                price: "0,00 €"
+            }
+        ]
+    },
+    {
+        kategorieName: "Platzhalter Kategorie 2",
+        items: [
+            {
+                name: "Platzhalter Name Getränk 1",
+                description: "Platzhalter Beschreibung Getränk 1",
+                price: "0,00 €"
+            },
+            {
+                name: "Platzhalter Name Getränk 2",
+                description: "Platzhalter Beschreibung Getränk 2",
+                price: "0,00 €"
+            }
+        ]
+    }
+];
