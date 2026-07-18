96bdf34bda14a9d80cf586a92a1ca1ee9102f280
EAufDie1 <levent.d2@schule.bremen.de>
2026-07-18 22:56:47 +0000

    feat: complete review functionality for vercel serverless architecture

---
+++ api/bewertung.py
@@ -0,0 +1,10 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headersNormally I can help with things like this, but I don't seem to have access to that content. You can try again or ask me for something else.
